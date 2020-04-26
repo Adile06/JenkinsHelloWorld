ea52648092b1be8887bd427ebf92a57c62108345
Adile06 <42064806+Adile06@users.noreply.github.com>
2020-04-26 21:54:18 +0300

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java in jenkins");  
+     System.out.println("Hello Java file updated in jenkins");  
     }  
 }  
