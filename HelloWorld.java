100b2a5002885acf07c0be9f23d91e10ac659b14
A Karci <adile.karci@hotmail.com>
2020-05-19 23:48:25 +0300

    deneme

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 class HelloWorld{  
     public static void main(String args[]){  
-     System.out.println("Hello Java in jenkins");  
+     System.out.println("Hello Java in jenkins!!!!!");  
+	 System.out.println("Java in jenkins!!!!!");  
+	 System.out.println("Jenkins!!!!!");  
     }  
 }  
